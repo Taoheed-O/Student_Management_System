04bf53a961d20a877e82fc7b93db377e4ead02e9
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-03-01 00:46:02 +0100

    student models added

--- students/models.py
+++ students/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    student_id = models.PositiveIntegerField()
+    firstname = models.CharField(max_length=25)
+    lastname = models.CharField(max_length=25)
+    email = models.EmailField()
+    faculty = models.CharField(max_length=50)
+    department = models.CharField(max_length=50)
+    cgpa = models.FloatField()
+    # image = models.ImageField(upload_to='passport')
