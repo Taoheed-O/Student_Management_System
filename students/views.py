59ca840fd62251d26abbb69a44b357b427ea9c41
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-03-13 09:16:42 +0100

    index view

--- students/views.py
+++ students/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from .models import Student
 # Create your views here.
 
 def index(request):
-    return render(request, "students/index.html")
+    students = Student.objects.all()
+    context = {"students":students}
+    return render(request, "students/index.html", context)
