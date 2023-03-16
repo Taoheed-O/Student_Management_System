425a29980958982c87edc28981ba9be99229c98f
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-03-17 00:16:30 +0100

    student individual view

--- students/urls.py
+++ students/urls.py
@@ -7,4 +7,5 @@ app_name = "students"
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:pk>',views.view_student, name='view_student'),
 ]
--- students/views.py
+++ students/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from .models import Student
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 def index(request):
     students = Student.objects.all()
     context = {"students":students}
     return render(request, "students/index.html", context)
+
+
+def view_student(request, id):
+    student = Student.objects.get(pk=id)
+    return HttpResponseRedirect(reverse("index"))
